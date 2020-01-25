8bdfbbea92557e2d38b87f3528df69443eee8b27
Tyler Yates <tyates907@gmail.com>
2020-01-24 17:52:34 -0800

    add controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,44 @@
 def UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You've successfully signed up!"
+      session[:user_id] = @user.id
+      redirect_to "/albums"
+    else
+      flash[:alert] = "There was a problem signing up."
+      redirect_to '/signup'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+  @user = User.find(params[:id])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to "/users/#{@user.id}"
+  else
+    redirect_to "/users/#{@user.id}/edit"
+  end
+end
+
+private
+def user_params
+  params.require(:user).permit(:email, :username, :password, :password_confirmation, :admin)
+end
+
+end
